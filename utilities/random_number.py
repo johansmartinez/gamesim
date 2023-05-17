19fd1eb0434d2bc80d5b31736b8fba67cca06463
AvellaLuisMiguel <avellaluismiguel@gmail.com>
2023-05-16 23:49:49 -0400

    Creacion de numero pseudoaleatorio

---
+++ utilities/random_number.py
@@ -0,0 +1,21 @@
+class RandomNumber():
+
+    def __init__(self):
+        self.a=9
+        self.c=3
+        self.m=256
+        self.xi=4
+        self.min=0
+        self.max=1
+
+    def calculate_ri(self):
+        self.xi= ((self.a*self.xi)+self.c)%self.m
+        print("asdasdasd "+str(self.xi))
+        return self.xi/(self.m-1)
+    
+    def calculate_ni(self):
+        return self.min+(self.max-self.min)*self.calculate_ri()
+    
+t=RandomNumber()
+for i in range(1,20):
+    print(str(t.calculate_ni()))
